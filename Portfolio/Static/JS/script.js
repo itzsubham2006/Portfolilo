f61ce91bb7f37873ad64e4d36637ba16f2e8af11
Subhamm_ <lastw5232@gmail.com>
2025-12-01 19:43:36 +0530

    onclick

--- Portfolio/Static/JS/script.js
+++ Portfolio/Static/JS/script.js
@@ -1,1 +1,12 @@
+var taplinks = document.getElementsByClassName('links')
+var tapcontents = document.getElementsByClassName('contents')
 
+
+function open(tapname){
+    for(taplink of taplinks){
+        taplink.classList.remove('active-link');   
+    }
+    for(tapcontent of tapcontents){
+        tapcontent.classList.remove('skill');   
+    }
+}
